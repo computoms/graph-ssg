a4c3132d67d9b509aa833a654c4dd41e67f1a2b7
thomas <thomas@computoms.com>
2022-05-28 22:06:31 -0400

    Separates and improves tests

--- src/tests/test_filemanager.py
+++ src/tests/test_filemanager.py
@@ -12,97 +12,83 @@ class DiskMock(Disk):
         return ['file1.md', 'file2.md']
 
 class TestFileManager:
+    fm = FileManager('input', 'output', 'templates')
+    file = ArticleFile('test', 'input/test.md', 'output/test.html')
+
     def test_WhenCreateArticleFile_ThenReturnsValidArticleFile(self):
-        fm = FileManager('input', 'output', 'templates')
-        article = fm.create_article_file('test')
+        article = self.fm.create_article_file('test')
         assert article.name == 'test'
         assert article.source == 'input/test.md'
         assert article.output == 'output/test.html'
 
     def test_WhenCreationTemplateFile_ThenReturnsValidTemplateFile(self):
-        fm = FileManager('input', 'output', 'templates')
-        template = fm.create_template_file('test', 'test_template.html')
+        template = self.fm.create_template_file('test', 'test_template.html')
         assert template.name == 'test'
         assert template.source == 'templates/test_template.html'
         assert template.output == 'output/test.html'
         assert template.is_template
 
     def test_WithNoFilter_WhenApplyFilter_ReturnsTrue(self):
-        fm = FileManager('intput', 'output', 'templates')
-        fm.file_filters = []
-        assert fm.apply_filter('test') == True
+        self.fm.file_filters = []
+        assert self.fm.apply_filter('test') == True
 
     def test_WithMatchingFilter_WhenApplyingFilter_ThenReturnsFalse(self):
-        fm = FileManager('intput', 'output', 'templates')
-        fm.file_filters = ['test']
-        assert fm.apply_filter('test') == False
+        self.fm.file_filters = ['test']
+        assert self.fm.apply_filter('test') == False
 
     def test_WithNonMatchingFilter_WhenApplyingFilter_ThenReturnsFalse(self):
-        fm = FileManager('intput', 'output', 'templates')
-        fm.file_filters = ['other']
-        assert fm.apply_filter('test') == True
+        self.fm.file_filters = ['other']
+        assert self.fm.apply_filter('test') == True
 
     def test_WithMockedDisk_WhenListSource_ThenReturnsValidArticleFiles(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk = DiskMock()
-        result = fm.list_source()
+        self.fm.disk = DiskMock()
+        result = self.fm.list_source()
         assert len(result) == 2
         assert result[0].name == 'file1'
         assert result[0].source == 'input/file1.md'
         assert result[0].output == 'output/file1.html'
 
     def test_WhenExists_ThenDiskIsCalledWithValidPath(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.file_exists = MagicMock()
-        fm.exists('test')
-        fm.disk.file_exists.assert_called_with('input/test.md')
+        self.fm.disk.file_exists = MagicMock()
+        self.fm.exists('test')
+        self.fm.disk.file_exists.assert_called_with('input/test.md')
 
     def test_WithInexistantFile_WhenGetSourceContent_ReturnsEmptyString(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.file_exists = MagicMock(return_value=False)
-        assert fm.get_source_content(ArticleFile('test', 'test.md', 'test.html')) == ['']
+        self.fm.disk.file_exists = MagicMock(return_value=False)
+        assert self.fm.get_source_content(self.file) == ['']
 
     def test_WhenGetSourceContent_ThenCorrectFileIsCalled(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.file_exists = MagicMock(return_value=True)
-        fm.disk.read_file = MagicMock()
-        fm.get_source_content(ArticleFile('test', 'input/test.md', 'output/test.html'))
-        fm.disk.read_file.assert_called_with('input/test.md')
+        self.fm.disk.file_exists = MagicMock(return_value=True)
+        self.fm.disk.read_file = MagicMock()
+        self.fm.get_source_content(self.file)
+        self.fm.disk.read_file.assert_called_with('input/test.md')
 
     def test_WhenSetSourceContent_ThenCorrectFileIsSet(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.write_to_file = MagicMock()
-        fm.set_source_content(ArticleFile('test', 'input/test.md', 'output/test.html'), 'hello')
-        fm.disk.write_to_file.assert_called_with('input/test.md', 'hello')
+        self.fm.disk.write_to_file = MagicMock()
+        self.fm.set_source_content(self.file, 'hello')
+        self.fm.disk.write_to_file.assert_called_with('input/test.md', 'hello')
 
     def test_WhenSaveOutput_ThenCorrectFileIsSet(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.write_to_file = MagicMock()
-        fm.save_output(ArticleFile('test', 'input/test.md', 'output/test.html'), 'hello')
-        fm.disk.write_to_file.assert_called_with('output/test.html', 'hello')
+        self.fm.disk.write_to_file = MagicMock()
+        self.fm.save_output(self.file, 'hello')
+        self.fm.disk.write_to_file.assert_called_with('output/test.html', 'hello')
 
     def test_WhenSaveOutput_ThenDatabaseIsUpdated(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.write_to_file = MagicMock()
-        fm.state_monitor.update = MagicMock()
-        file = ArticleFile('test', 'input/test.md', 'output/test.html')
-        fm.save_output(file, 'hello')
-        fm.state_monitor.update.assert_called_with(file)
+        self.fm.disk.write_to_file = MagicMock()
+        self.fm.state_monitor.update = MagicMock()
+        self.fm.save_output(self.file, 'hello')
+        self.fm.state_monitor.update.assert_called_with(self.file)
 
     def test_WithExistingFile_WhenDeleteArticle_ThenFileIsDeleted(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.file_exists = MagicMock(return_value=True)
-        fm.disk.remove_file = MagicMock()
-        file = ArticleFile('test', 'input/test.md', 'output/test.html')
-        fm.delete_article(file)
-        fm.disk.remove_file.assert_called()
+        self.fm.disk.file_exists = MagicMock(return_value=True)
+        self.fm.disk.remove_file = MagicMock()
+        self.fm.delete_article(self.file)
+        self.fm.disk.remove_file.assert_called()
 
     def test_WhenDeleteArticle_ThenDatabaseIsUpdated(self):
-        fm = FileManager('input', 'output', 'templates')
-        fm.disk.file_exists = MagicMock(return_value=False)
-        fm.disk.remove_file = MagicMock()
-        fm.state_monitor.remove = MagicMock()
-        file = ArticleFile('test', 'input/test.md', 'output/test.html')
-        fm.delete_article(file)
-        fm.state_monitor.remove.assert_called_with(file)
+        self.fm.disk.file_exists = MagicMock(return_value=False)
+        self.fm.disk.remove_file = MagicMock()
+        self.fm.state_monitor.remove = MagicMock()
+        self.fm.delete_article(self.file)
+        self.fm.state_monitor.remove.assert_called_with(self.file)
 
